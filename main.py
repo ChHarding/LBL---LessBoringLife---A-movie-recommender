3884b1f01ccc416556603e862bbe439693bef25d
Melika Ziba <145093756+MeliZiba@users.noreply.github.com>
2023-10-21 13:06:42 -0500

    Update main.py
    In the new version, each window will close when the new window opens.
    
    The code works fine and returns appropriate results in each step.
    
    Still have issue with pagination

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import tkinter as tk
-from tkinter import ttk
-from gui import MovieRecommenderApp
+from tkinter import messagebox
+from tmdb_helper import search_movies
 
-def main():
-    root = tk.Tk()
-    app = MovieRecommenderApp(root)
-    root.mainloop()
+# Initializing the Tkinter app
+app = tk.Tk()
+app.geometry("400x200")
+app.title("LessBoringLife App")
 
-if __name__ == "__main__":
-    main()
+# Step 1: Input for the user to enter the API key
+api_key_label = tk.Label(app, text="Enter your TMDB API Key: ", font="Calibri 10 bold")
+api_key_label.pack()
+api_key_entry = tk.Entry(app, width=50, show='*')  # Masking the input with asterisks
+api_key_entry.pack()
+
+# Step 2: Input for the user to enter a movie name
+movie_name_label = tk.Label(app, text="Enter a movie name: ", font="Calibri 10 bold")
+movie_name_label.pack()
+movie_name_entry = tk.Entry(app, width=50)
+movie_name_entry.pack()
+
+# Run the main loop for the application
+search_button = tk.Button(app, text="Search Movies", command=search_movies)
+search_button.pack()
+app.mainloop()
