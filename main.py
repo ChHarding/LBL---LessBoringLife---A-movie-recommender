a7f68a148567fab2bb142b8d37f755d1bbbcfdf4
Melika Ziba <145093756+MeliZiba@users.noreply.github.com>
2023-11-06 00:31:06 -0600

    Update main.py
    The API entry removed form the code

--- main.py
+++ main.py
@@ -7,12 +7,6 @@ app = tk.Tk()
 app.geometry("400x200")
 app.title("LessBoringLife App")
 
-# Step 1: Input for the user to enter the API key
-api_key_label = tk.Label(app, text="Enter your TMDB API Key: ", font="Calibri 10 bold")
-api_key_label.pack()
-api_key_entry = tk.Entry(app, width=50, show='*')  # Masking the input with asterisks
-api_key_entry.pack()
-
 # Step 2: Input for the user to enter a movie name
 movie_name_label = tk.Label(app, text="Enter a movie name: ", font="Calibri 10 bold")
 movie_name_label.pack()
@@ -20,6 +14,6 @@ movie_name_entry = tk.Entry(app, width=50)
 movie_name_entry.pack()
 
 # Run the main loop for the application
-search_button = tk.Button(app, text="Search Movies", command=lambda: search_movies(app, api_key_entry, movie_name_entry))
+search_button = tk.Button(app, text="Search Movies", command=lambda: search_movies(app, movie_name_entry))
 search_button.pack()
 app.mainloop()
